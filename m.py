d2add31a09b2fa2332e9ed1c7440159f4cbf4e0c
ankushnirwan1 <133894685+ankushnirwan1@users.noreply.github.com>
2024-07-24 17:27:17 +0530

    Add files via upload

--- m.py
+++ m.py
@@ -11,7 +11,7 @@ import os
 bot = telebot.TeleBot('7284045723:AAE572P-NS8icyO_Vo30J3gIAHka1fweXOA')
 
 # Admin user IDs
-admin_id = ["6211690957,1351184742"]
+admin_id = ["6211690957"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -246,9 +246,9 @@ def handle_bgmi(message):
                 subprocess.run(full_command, shell=True)
                 response = f"**BGMI Attack Finished. Target: {target} Port: {port} Time: {time}**"
         else:
-            response = "**Usage :- /bgmi <target> <port> <time>\n\nJoin For More Hacks - @RINOMODSOFFICIAL**"  # Updated command syntax
+            response = "**Usage :- /bgmi <target> <port> <time>**\n\n**Join For More Hacks - @RINOMODSOFFICIAL**"  # Updated command syntax
     else:
-        response = "**You Are Not Authorised To Use This Bot\n\nMessage To Buy - @OFFICIALRINO**"
+        response = "**You Are Not Authorised To Use This Bot**\n\n**Message To Buy - @OFFICIALRINO**"
 
     bot.reply_to(message, response)
 
@@ -307,12 +307,7 @@ def welcome_start(message):
 @bot.message_handler(commands=['rules'])
 def welcome_rules(message):
     user_name = message.from_user.first_name
-    response = f'''**{user_name} Please Follow These Rules:
-
-1. Dont Run Too Many Attacks !! Cause A Ban From Bot
-2. Dont Run 2 Attacks At Same Time Becz If U Then U Got Banned From Bot. 
-3. We Daily Checks The Logs So Follow these rules to avoid Ban!!
-Join - @RINOMODSOFFICIAL**'''
+    response = f'''**{user_name} Please Follow These Rules\n\n1) Dont Run Too Many Attacks !! Cause A Ban From Bot\n2) Dont Run 2 Attacks At Same Time Becz If U Then U Got Banned From Bot.\n3) We Daily Checks The Logs So Follow these rules to avoid Ban.\nJoin - @RINOMODSOFFICIAL**'''
     bot.reply_to(message, response)
 
 @bot.message_handler(commands=['plan'])
@@ -336,16 +331,7 @@ Month-->1600 Rs
 @bot.message_handler(commands=['admincmd'])
 def welcome_plan(message):
     user_name = message.from_user.first_name
-    response = f'''{user_name}, Admin Commands Are Here!!:
-
-/add <userId> : Add a User.
-/remove <userid> Remove a User.
-/allusers : Authorised Users Lists.
-/logs : All Users Logs.
-/broadcast : Broadcast a Message.
-/clearlogs : Clear The Logs File.
-Join - @RINOMODSOFFICIAL
-'''
+    response = f'''**{user_name}, Admin Commands Are Here\n/add <userId> : Add a User.\n/remove <userid> Remove a User.\n/allusers : Authorised Users Lists.\n/logs : All Users Logs.\n/broadcast : Broadcast a Message.\n/clearlogs : Clear The Logs File.\nJoin - @RINOMODSOFFICIAL**'''
     bot.reply_to(message, response)
 
 
